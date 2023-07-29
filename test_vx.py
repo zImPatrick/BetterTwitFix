0c66a2c91866f01d1a0a3d104dbdb6795e66d5ba
Dylan <dylanpdx@gmail.com>
2023-07-29 20:07:10 +0100

    Fixed tests.. Again.

--- test_vx.py
+++ test_vx.py
@@ -6,13 +6,15 @@ import msgs
 from flask.testing import FlaskClient
 client = FlaskClient(twitfix.app)
 
+twitterAccountName='X' # the fact that I have to do this in the rare chance that they change their username back is stupid
+
 testUser="https://twitter.com/jack"
 testUserID = "https://twitter.com/i/user/12"
 testUserWeirdURLs=["https://twitter.com/jack?lang=en","https://twitter.com/jack/with_replies","https://twitter.com/jack/media","https://twitter.com/jack/likes","https://twitter.com/jack/with_replies?lang=en","https://twitter.com/jack/media?lang=en","https://twitter.com/jack/likes?lang=en","https://twitter.com/jack/"]
 testTextTweet="https://twitter.com/jack/status/20"
 testVideoTweet="https://twitter.com/Twitter/status/1263145271946551300"
 testMediaTweet="https://twitter.com/Twitter/status/1118295916874739714"
-testMultiMediaTweet="https://twitter.com/Twitter/status/1390026628957417473"
+testMultiMediaTweet="https://twitter.com/Twitter/status/1154172324599537665"
 testPollTweet="https://twitter.com/norm/status/651169346518056960"
 testQRTTweet="https://twitter.com/Twitter/status/1232823570046255104"
 testQrtCeptionTweet="https://twitter.com/CatherineShu/status/585253766271672320"
@@ -23,9 +25,9 @@ testQrtVideoTweet="https://twitter.com/Twitter/status/1494436688554344449"
 testNSFWTweet="https://twitter.com/kuyacoy/status/1581185279376838657"
 
 textVNF_compare = {'tweet': 'https://twitter.com/jack/status/20', 'url': '', 'description': 'just setting up my twttr', 'screen_name': 'jack', 'type': 'Text', 'images': ['', '', '', '', ''], 'time': 'Tue Mar 21 20:50:14 +0000 2006', 'qrtURL': None, 'nsfw': False}
-videoVNF_compare={'tweet': 'https://twitter.com/Twitter/status/1263145271946551300', 'url': 'https://video.twimg.com/amplify_video/1263145212760805376/vid/1280x720/9jous8HM0_duxL0w.mp4?tag=13', 'description': 'Testing, testing...\n\nA new way to have a convo with exactly who you want. We’re starting with a small % globally, so keep your 👀 out to see it in action. https://t.co/pV53mvjAVT', 'thumbnail': 'https://pbs.twimg.com/media/EYeX7akWsAIP1_1.jpg', 'screen_name': 'Twitter', 'type': 'Video', 'images': ['', '', '', '', ''], 'time': 'Wed May 20 16:31:15 +0000 2020', 'qrtURL': None, 'nsfw': False,'verified': True, 'size': {'width': 1920, 'height': 1080}}
-testMedia_compare={'tweet': 'https://twitter.com/Twitter/status/1118295916874739714', 'url': '', 'description': 'On profile pages, we used to only show someone’s replies, not the original Tweet 🙄 Now we’re showing both so you can follow the conversation more easily! https://t.co/LSBEZYFqmY', 'thumbnail': 'https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg', 'screen_name': 'Twitter', 'type': 'Image', 'images': ['https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg', '', '', '', '1'], 'time': 'Tue Apr 16 23:31:38 +0000 2019', 'qrtURL': None, 'nsfw': False, 'size': {}}
-testMultiMedia_compare={'tweet': 'https://twitter.com/Twitter/status/1390026628957417473', 'url': '', 'description': 'no bird too tall, no crop too short\n\nintroducing bigger and better images on iOS and Android, now available to everyone https://t.co/2buHfhfRAx', 'thumbnail': 'https://pbs.twimg.com/media/E0pd2L2XEAQ_gnn.jpg', 'screen_name': 'Twitter', 'type': 'Image', 'images': ['https://pbs.twimg.com/media/E0pd2L2XEAQ_gnn.jpg', 'https://pbs.twimg.com/media/E0pd2hPXoAY9-TZ.jpg', '', '', '2'], 'qrtURL': None, 'nsfw': False, 'verified': True, 'size': {}}
+videoVNF_compare={'tweet': 'https://twitter.com/Twitter/status/1263145271946551300', 'url': 'https://video.twimg.com/amplify_video/1263145212760805376/vid/1280x720/9jous8HM0_duxL0w.mp4?tag=13', 'description': 'Testing, testing...\n\nA new way to have a convo with exactly who you want. We’re starting with a small % globally, so keep your 👀 out to see it in action. https://t.co/pV53mvjAVT', 'thumbnail': 'https://pbs.twimg.com/media/EYeX7akWsAIP1_1.jpg', 'screen_name': twitterAccountName, 'type': 'Video', 'images': ['', '', '', '', ''], 'time': 'Wed May 20 16:31:15 +0000 2020', 'qrtURL': None, 'nsfw': False,'verified': True, 'size': {'width': 1920, 'height': 1080}}
+testMedia_compare={'tweet': 'https://twitter.com/Twitter/status/1118295916874739714', 'url': '', 'description': 'On profile pages, we used to only show someone’s replies, not the original Tweet 🙄 Now we’re showing both so you can follow the conversation more easily! https://t.co/LSBEZYFqmY', 'thumbnail': 'https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg', 'screen_name': twitterAccountName, 'type': 'Image', 'images': ['https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg', '', '', '', '1'], 'time': 'Tue Apr 16 23:31:38 +0000 2019', 'qrtURL': None, 'nsfw': False, 'size': {}}
+testMultiMedia_compare={'tweet': 'https://twitter.com/Twitter/status/1154172324599537665', 'url': '', 'description': '10 days. 40 cities. Virtual #Tweetups are happening around the world. Join us and follow the convo! \nhttp://sharedstudios.com/tweetups https://t.co/M436G4fVio', 'thumbnail': 'https://pbs.twimg.com/media/EARxm9FXYAA8qqz.jpg', 'screen_name': twitterAccountName, 'type': 'Image', 'images': ['https://pbs.twimg.com/media/EARxm9FXYAA8qqz.jpg', 'https://pbs.twimg.com/media/EARxm9GW4AEnQkp.jpg', 'https://pbs.twimg.com/media/EARxm9HXkAEEsM0.jpg', 'https://pbs.twimg.com/media/EARxm9QXkAAZIA4.jpg', '4'], 'qrtURL': None, 'nsfw': False, 'verified': True, 'size': {}}
 
 testPoll_comparePoll={"name":"poll2choice_text_only","binding_values":{"choice1_label":{"type":"STRING","string_value":"Mean one thing"},"choice2_label":{"type":"STRING","string_value":"Mean multiple things"},"end_datetime_utc":{"type":"STRING","string_value":"2015-10-06T22:57:24Z"},"counts_are_final":{"type":"BOOLEAN","boolean_value":True},"choice2_count":{"type":"STRING","string_value":"33554"},"choice1_count":{"type":"STRING","string_value":"124875"},"last_updated_datetime_utc":{"type":"STRING","string_value":"2015-10-06T22:57:31Z"},"duration_minutes":{"type":"STRING","string_value":"1440"}}}
 testPoll_comparePollVNF={'total_votes': 158429, 'choices': [{'text': 'Mean one thing', 'votes': 124875, 'percent': 78.8}, {'text': 'Mean multiple things', 'votes': 33554, 'percent': 21.2}]}
@@ -72,7 +74,7 @@ def test_UserExtractWeirdURLs():
 def test_videoTweetExtract():
     tweet = twExtract.extractStatus(testVideoTweet)
     assert tweet["full_text"]==videoVNF_compare['description']
-    assert tweet["user"]["screen_name"]=="Twitter"
+    assert tweet["user"]["screen_name"]==twitterAccountName
     assert 'extended_entities' in tweet
     assert len(tweet['extended_entities']["media"])==1
     video = tweet['extended_entities']["media"][0]
@@ -83,7 +85,7 @@ def test_videoTweetExtract():
 def test_mediaTweetExtract():
     tweet = twExtract.extractStatus(testMediaTweet)
     assert tweet["full_text"]==testMedia_compare['description']
-    assert tweet["user"]["screen_name"]=="Twitter"
+    assert tweet["user"]["screen_name"]==twitterAccountName
     assert 'extended_entities' in tweet
     assert len(tweet['extended_entities']["media"])==1
     video = tweet['extended_entities']["media"][0]
@@ -94,14 +96,14 @@ def test_mediaTweetExtract():
 def test_multimediaTweetExtract():
     tweet = twExtract.extractStatus(testMultiMediaTweet)
     assert tweet["full_text"]==testMultiMedia_compare['description']
-    assert tweet["user"]["screen_name"]=="Twitter"
+    assert tweet["user"]["screen_name"]==twitterAccountName
     assert 'extended_entities' in tweet
-    assert len(tweet['extended_entities']["media"])==2
+    assert len(tweet['extended_entities']["media"])==4
     video = tweet['extended_entities']["media"][0]
-    assert video["media_url_https"]=="https://pbs.twimg.com/media/E0pd2L2XEAQ_gnn.jpg"
+    assert video["media_url_https"]=="https://pbs.twimg.com/media/EARxm9FXYAA8qqz.jpg"
     assert video["type"]=="photo"
     video = tweet['extended_entities']["media"][1]
-    assert video["media_url_https"]=="https://pbs.twimg.com/media/E0pd2hPXoAY9-TZ.jpg"
+    assert video["media_url_https"]=="https://pbs.twimg.com/media/EARxm9GW4AEnQkp.jpg"
     assert video["type"]=="photo"
 
 def test_pollTweetExtract():
@@ -211,11 +213,11 @@ def test_veryLongEmbed():
     assert resp.status_code==200
 
 def test_embedFromOutdatedCache(): # presets a cache that has VNF's with missing fields; there's probably a better way to do this
-    cache.setCache({"https://twitter.com/Twitter/status/1118295916874739714":{"description":"On profile pages, we used to only show someone’s replies, not the original Tweet 🙄 Now we’re showing both so you can follow the conversation more easily! https://t.co/LSBEZYFqmY","hits":0,"images":["https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg","","","","1"],"likes":5033,"nsfw":False,"pfp":"https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_normal.jpg","qrt":{},"rts":754,"screen_name":"Twitter","thumbnail":"https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg","time":"Tue Apr 16 23:31:38 +0000 2019","tweet":"https://twitter.com/Twitter/status/1118295916874739714","type":"Image","uploader":"Twitter","url":""},
-            "https://twitter.com/Twitter/status/1263145271946551300":{"description":"Testing, testing...\n\nA new way to have a convo with exactly who you want. We’re starting with a small % globally, so keep your 👀 out to see it in action. https://t.co/pV53mvjAVT","hits":0,"images":["","","","",""],"likes":61584,"nsfw":False,"pfp":"https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_normal.jpg","qrt":{},"rts":17138,"screen_name":"Twitter","thumbnail":"https://pbs.twimg.com/media/EYeX7akWsAIP1_1.jpg","time":"Wed May 20 16:31:15 +0000 2020","tweet":"https://twitter.com/Twitter/status/1263145271946551300","type":"Video","uploader":"Twitter","url":"https://video.twimg.com/amplify_video/1263145212760805376/vid/1280x720/9jous8HM0_duxL0w.mp4?tag=13"},
+    cache.setCache({"https://twitter.com/Twitter/status/1118295916874739714":{"description":"On profile pages, we used to only show someone’s replies, not the original Tweet 🙄 Now we’re showing both so you can follow the conversation more easily! https://t.co/LSBEZYFqmY","hits":0,"images":["https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg","","","","1"],"likes":5033,"nsfw":False,"pfp":"https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_normal.jpg","qrt":{},"rts":754,"screen_name":twitterAccountName,"thumbnail":"https://pbs.twimg.com/media/D4TS4xeX4AA02DI.jpg","time":"Tue Apr 16 23:31:38 +0000 2019","tweet":"https://twitter.com/Twitter/status/1118295916874739714","type":"Image","uploader":twitterAccountName,"url":""},
+            "https://twitter.com/Twitter/status/1263145271946551300":{"description":"Testing, testing...\n\nA new way to have a convo with exactly who you want. We’re starting with a small % globally, so keep your 👀 out to see it in action. https://t.co/pV53mvjAVT","hits":0,"images":["","","","",""],"likes":61584,"nsfw":False,"pfp":"https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_normal.jpg","qrt":{},"rts":17138,"screen_name":twitterAccountName,"thumbnail":"https://pbs.twimg.com/media/EYeX7akWsAIP1_1.jpg","time":"Wed May 20 16:31:15 +0000 2020","tweet":"https://twitter.com/Twitter/status/1263145271946551300","type":"Video","uploader":twitterAccountName,"url":"https://video.twimg.com/amplify_video/1263145212760805376/vid/1280x720/9jous8HM0_duxL0w.mp4?tag=13"},
             #"https://twitter.com/Twitter/status/1293239745695211520":{"description":"We tested, you Tweeted, and now we’re rolling it out to everyone! https://t.co/w6Q3Q6DiKz","hits":0,"images":["https://pbs.twimg.com/media/EfJ-C-JU0AAQL_C.jpg","https://pbs.twimg.com/media/EfJ-aHlU0AAU1kq.jpg","","","2"],"likes":5707,"nsfw":False,"pfp":"https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_normal.jpg","qrt":{},"rts":1416,"screen_name":"Twitter","thumbnail":"https://pbs.twimg.com/media/EfJ-C-JU0AAQL_C.jpg","time":"Tue Aug 11 17:35:57 +0000 2020","tweet":"https://twitter.com/Twitter/status/1293239745695211520","type":"Image","uploader":"Twitter","url":""},
             "https://twitter.com/jack/status/20":{"description":"just setting up my twttr","hits":0,"images":["","","","",""],"likes":179863,"nsfw":False,"pfp":"https://pbs.twimg.com/profile_images/1115644092329758721/AFjOr-K8_normal.jpg","qrt":{},"rts":122021,"screen_name":"jack","thumbnail":"","time":"Tue Mar 21 20:50:14 +0000 2006","tweet":"https://twitter.com/jack/status/20","type":"Text","uploader":"jack","url":""},
-            testQrtVideoTweet:{'tweet': 'https://twitter.com/Twitter/status/1494436688554344449', 'url': '', 'description': 'https://twitter.com/TwitterSupport/status/1494386367467593737', 'thumbnail': '', 'uploader': 'Twitter', 'screen_name': 'Twitter', 'pfp': 'https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_normal.jpg', 'type': 'Text', 'images': ['', '', '', '', ''], 'likes': 5186, 'rts': 703, 'time': 'Thu Feb 17 22:20:46 +0000 2022', 'qrt': {'desc': 'Keep your fave DM convos easily accessible by pinning them! You can now pin up to six conversations that will stay at the top of your DM inbox.\n\nAvailable on Android, iOS, and web. https://t.co/kIjlzf9XLJ', 'handle': 'Twitter Support', 'screen_name': 'TwitterSupport', 'verified': True, 'id': '1494386367467593737'}, 'nsfw': False, 'verified': True, 'size': {}}
+            testQrtVideoTweet:{'tweet': 'https://twitter.com/Twitter/status/1494436688554344449', 'url': '', 'description': 'https://twitter.com/TwitterSupport/status/1494386367467593737', 'thumbnail': '', 'uploader': twitterAccountName, 'screen_name': twitterAccountName, 'pfp': 'https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_normal.jpg', 'type': 'Text', 'images': ['', '', '', '', ''], 'likes': 5186, 'rts': 703, 'time': 'Thu Feb 17 22:20:46 +0000 2022', 'qrt': {'desc': 'Keep your fave DM convos easily accessible by pinning them! You can now pin up to six conversations that will stay at the top of your DM inbox.\n\nAvailable on Android, iOS, and web. https://t.co/kIjlzf9XLJ', 'handle': 'Twitter Support', 'screen_name': 'TwitterSupport', 'verified': True, 'id': '1494386367467593737'}, 'nsfw': False, 'verified': True, 'size': {}}
             })
     #embed time
     resp = client.get(testTextTweet.replace("https://twitter.com",""),headers={"User-Agent":"test"})
