0382b4e68103bfcf7944f3026c5e145355e6eed8
Dylan <dylanpdx@gmail.com>
2023-12-16 17:07:01 +0000

    New guest token get method

--- twExtract/__init__.py
+++ twExtract/__init__.py
@@ -19,7 +19,7 @@ v2graphql_api="2OOZWmw8nAtUHVnXXQhgaA"
 
 v2AnonFeatures='{"creator_subscriptions_tweet_preview_api_enabled":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"responsive_web_home_pinned_timelines_enabled":true,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"responsive_web_media_download_video_enabled":false,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_enhance_cards_enabled":false}'
 v2AnonGraphql_api="5GOHgZe-8U2j5sVHQzEm9A"
-
+gt_pattern = r'document\.cookie="gt=([^;]+);'
 class TwExtractError(Exception):
     def __init__(self, code, message):
         self.code = code
@@ -32,8 +32,14 @@ def getGuestToken():
     global guestToken
     global guestTokenUses
     if guestToken is None:
-        r = requests.post("https://api.twitter.com/1.1/guest/activate.json", headers={"Authorization":v2Bearer})
-        guestToken = json.loads(r.text)["guest_token"]
+        r = requests.get("https://twitter.com",headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0"})
+        m = re.search(gt_pattern, r.text)
+        if m is None:
+            r = requests.post("https://api.twitter.com/1.1/guest/activate.json", headers={"Authorization":v2Bearer})
+            guestToken = json.loads(r.text)["guest_token"]
+        else:
+            guestToken = m.group(1)
+        guestTokenUses = 0
     else:
         guestTokenUses+=1
         if guestTokenUses > 10:
